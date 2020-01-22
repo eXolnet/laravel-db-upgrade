09d6da42a133036dc94de157dbd31035ccc2e87c
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2020-01-22 09:52:18 -0500

    Publish the default configuration file

--- src/DbUpgradeServiceProvider.php
+++ src/DbUpgradeServiceProvider.php
@@ -7,6 +7,20 @@ use Illuminate\Support\ServiceProvider;
 class DbUpgradeServiceProvider extends ServiceProvider
 {
     /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->publishes([
+            __DIR__.'/../config/db-upgrade.php' => config_path('db-upgrade.php'),
+        ], 'config');
+    }
+
+    /**
      * Register the application services.
      */
     public function register()
